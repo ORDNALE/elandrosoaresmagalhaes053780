154a57e5cceb4dbadd3e53f846ffc4a152805897
Elandro Soares <elandro.soares@gmail.com>
2026-01-26 20:53:10 -0400

    chore: ajustar infraestrutura Docker, propriedades e OpenAPI

--- src/main/java/com/elandroapi/core/config/OpenApiConfig.java
+++ src/main/java/com/elandroapi/core/config/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.elandroapi.core.config;
 
+import jakarta.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -12,11 +14,13 @@ import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
         description = "API para gerenciamento de álbuns, artistas e capas"
     )
 )
-@SecurityScheme(
-    securitySchemeName = "jwt",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT"
+@SecuritySchemes(
+    @SecurityScheme(
+        securitySchemeName = "jwt",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+    )
 )
-public class OpenApiConfig {
+public class OpenApiConfig extends Application {
 }
